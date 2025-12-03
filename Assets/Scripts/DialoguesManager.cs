354508c9662da6e56bd14088a553d0ee0843899a
Cristian067 <cristian.n.c005@gmail.com>
2025-12-03 10:10:11 +0100

    Tutorial+can complete levels

--- Assets/Scripts/DialoguesManager.cs
+++ Assets/Scripts/DialoguesManager.cs
@@ -72,7 +72,10 @@ public class DialoguesManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        if(dialogues[0].dialogueName == "Start")
+        {
+            StartDialogue(0);
+        }
     }
 
     // Update is called once per frame
